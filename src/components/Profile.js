84d680d05314642d804bb59643799968f6f6dd27
François Ulrich <fulrich3@gmail.com>
2020-08-30 18:04:42 +0200

    home: affichage des infos de l'utilisateur connecté

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-import { withStyles } from '@material-ui/core/styles';
+import dayjs from 'dayjs';
 
 // MUI stuff
-import Card from '@material-ui/core/Card';
+import { withStyles } from '@material-ui/core/styles';
+
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import MuiLink from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+
+// Mui Icons
+import LocationOn from '@material-ui/icons/LocationOn';
+import InsertLink from '@material-ui/icons/InsertLink';
+import CalendarToday from '@material-ui/icons/CalendarToday';
 
 // Redux
 import { connect } from 'react-redux';
@@ -16,26 +23,21 @@ import { connect } from 'react-redux';
 const Link = require("react-router-dom").Link;
 
 
+
 // import Card from '@material-ui/core/Card';
 // import CardContent from '@material-ui/core/CardContent';
 // import CardMedia from '@material-ui/core/CardMedia';
 // import { Typography } from '@material-ui/core';
 
 const styles = {
-    card:{
-        padding: '20px'
-    },
-    paragraph:{
-        textAlign: 'center'
-    },
-    buttonsContainer:{
-        textAlign: 'center'
-    },
     button:{
         margin: '10px'
     },
 
     profile: {
+        padding: '20px',
+        textAlign: 'center',
+
         '& .image-wrapper': {
             textAlign: 'center',
             position: 'relative',
@@ -92,19 +94,74 @@ export class Profile extends Component {
         } = this.props;
 
         const unauthenticatedContent = (
-            <Card className={classes.card}>
-                <p className={classes.paragraph}>No profile found, please sign up.</p>
+            <Paper className={classes.paper}>
+                <div className={classes.profile}>
+                    <Typography variant="body2" >No profile found, please sign up.</Typography>
 
-                <div className={classes.buttonsContainer}>
-                    <Button component={Link} to="/login" variant="contained" color="primary" className={classes.button}>Login</Button>
-                    <Button component={Link} to="/signup" variant="contained" color="secondary" className={classes.button}>Signup</Button>
+                    <div className={classes.buttonsContainer}>
+                        <Button component={Link} to="/login" variant="contained" color="primary" className={classes.button}>Login</Button>
+                        <Button component={Link} to="/signup" variant="contained" color="secondary" className={classes.button}>Signup</Button>
+                    </div>
                 </div>
-            </Card>
+            </Paper>
         );
 
         const authenticatedContent = (
             <Paper className={classes.paper}>
-                <p>Authenticated</p>
+                <div className={classes.profile}>
+                    <div className="image-wrapper">
+                        <img src={imageUrl} alt="profile" className="profile-image"/>
+                    </div>
+
+                    <hr/>
+
+                    <div className="profile-details">
+                        
+                        <MuiLink 
+                        component={Link}
+                        to={`/users/${handle}`}
+                        color="primary"
+                        variant="h5">
+                            @{handle}
+                        </MuiLink>
+
+                        <hr/>
+
+                        {bio && <Typography
+                            variant="body2"
+                        >{bio}</Typography>}
+
+                        <hr/>
+
+                        {location && (
+                            <Fragment>
+                                <LocationOn color="primary"/>
+                                {' '}
+                                <span>{location}</span>
+                            </Fragment>
+                        )}
+
+                        <hr/>
+
+                        {website && (
+                            <Fragment>
+                                <InsertLink color="primary"/>
+                                {' '}
+                                <a href={website} target="_blank" rel="noopener noreferrer">{website}</a>
+                            </Fragment>
+                        )}
+
+                        <hr/>
+
+                        {createdAt && (
+                            <Fragment>
+                                <CalendarToday color="primary"/>
+                                {' '}
+                                <span>Joined { dayjs(createdAt).format("MM/YYYY") }</span>
+                            </Fragment>
+                        )}
+                    </div>
+                </div>
             </Paper>
         );
 
